85385d2ad66978135969480c0e4afc73b7fa9717
mutebesi <63821642+mutebesi@users.noreply.github.com>
2026-05-10 00:11:34 +0100

    Update CountriesTest.php

--- tests/CountriesTest.php
+++ tests/CountriesTest.php
@@ -24,11 +24,13 @@ class CountriesTest extends TestCase
         $this->assertEquals('Nairobi', $country->capital);
     }
 
-    public function test_can_find_country_by_iso3()
+    public function test_premium_data_is_present()
     {
-        $country = Countries::find('KEN');
-        $this->assertInstanceOf(Country::class, $country);
-        $this->assertEquals('Kenya', $country->name);
+        $country = Countries::find('KE');
+        $this->assertGreaterThan(0, $country->population);
+        $this->assertNotEmpty($country->callingCode);
+        $this->assertNotEmpty($country->flagEmoji);
+        $this->assertNotEmpty($country->currencyCode());
     }
 
     public function test_can_search_countries()
@@ -47,12 +49,34 @@ class CountriesTest extends TestCase
         }
     }
 
+    public function test_premium_collection_filters()
+    {
+        $large = Countries::wherePopulationAbove(100000000);
+        $this->assertGreaterThan(0, count($large));
+        
+        $usd = Countries::whereCurrency('USD');
+        $this->assertGreaterThan(0, count($usd));
+        
+        $spanish = Countries::whereLanguage('spa');
+        $this->assertGreaterThan(0, count($spanish));
+    }
+
     public function test_can_generate_dropdown_options()
     {
         $options = Countries::dropdown();
         $this->assertIsArray($options);
         $this->assertArrayHasKey('label', $options[0]);
         $this->assertArrayHasKey('value', $options[0]);
+    }
+
+    public function test_collection_utilities()
+    {
+        $names = Countries::all()->pluck('name');
+        $this->assertIsArray($names);
+        $this->assertContains('Kenya', $names);
+        
+        $joined = Countries::whereContinent('Africa')->join(', ');
+        $this->assertStringContainsString('Kenya', $joined);
     }
 
     public function test_can_check_validity()
